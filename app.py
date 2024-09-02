44ea28d25758737de490df44da60881f3a24275d
ben-dh3 <benjaminduffield1997@gmail.com>
2024-09-02 15:55:22 +0100

    landing page and signup styling

--- app.py
+++ app.py
@@ -25,21 +25,13 @@ app.config['MAIL_USE_SSL'] = False
 
 mail.init_app(app)
 
-
-@app.route('/')
-def get_posts():
-    connection = get_flask_database_connection(app)
-    repository = PostRepository(connection)
-    posts = repository.all()
-    return render_template('index.html', posts=posts)
-
-# This route simply returns the login page
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+# return the landing page with login form and signin page link
+@app.route('/', methods=['GET', 'POST'])
+def landingpage():
     if request.method == 'GET':
         if not session.get("user_id"):
-            return render_template('login.html')
-        return redirect('/account_page')
+            return render_template('landingpage.html')
+        return redirect('/feed')
     else:
         connection = get_flask_database_connection(app)
         repository = UserRepository(connection)
@@ -50,25 +42,54 @@ def login():
             user = repository.find_by_email(email)
             # Set the user ID in session
             session['user_id'] = user.id
-            return redirect('/account_page')
+            return redirect('/feed')
         else:
-            return render_template('login_error.html')
+            # return the same page with errors populated
+            return redirect('/')
 
+# feed page shows posts, new post form, and logout button
+@app.route('/feed', methods=['GET', 'POST'])
+def feed():
+    if request.method == 'GET':
+        if 'user_id' not in session:
+        # No user id in the session so the user is not logged in.
+            return redirect('/')
+        else:
+            connection = get_flask_database_connection(app)
+            repository = PostRepository(connection)
+            posts = repository.all()
+            return render_template('feed.html', posts=posts)
+    else:
+        connection = get_flask_database_connection(app)
+        repository = PostRepository(connection)
+        # sanitize user input
+        message = sanitizer.sanitize(request.form.get('message'))
+        tag = sanitizer.sanitize(request.form.get('tag'))
+        email = repository.find_email_by_username(tag)
+        # create a post object
+        t = datetime.now()
+        time = t.strftime("%H:%M:%S")
+        user_id = session['user_id']
+        username = repository.find_username_with_userid(user_id)
+        post = Post(None, message, time, user_id, username)
+        # if another user is tagged in their post notify them with an email
+        if email is not None:
+            msg = Message(subject='You were tagged in a post on Chitter', body=f'{username} tagged you in their post. Here is what they wrote:\n\n{message}', sender = 'hello@chitter.com', recipients = [f'{email}'])
+            mail.send(msg)  
+        repository.create(post)
+        return redirect('/feed')
 
 @app.route('/logout')
 def logout():
     session.clear()
-    return render_template('logout.html')
+    # when logged out return to landing page
+    return redirect('/')
 
-# This route receives login information (email and password) as POST parameters,
-# checks whether the credentials are valid, and if so finds the user in the database
-# using the email. If all goes well, it stores the user's ID in the session
-# and shows a success page.
-
+# signup page
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
-        return render_template('signup.html')
+        return render_template('signup2.html')
     else:
         connection = get_flask_database_connection(app)
         repository = UserRepository(connection)
@@ -92,44 +113,6 @@ def signup():
             repository.create(user)
             session['user_id'] = user.id
             return render_template('signup_success.html')
-
-
-# This route is an example of a "authenticated-only" route. It can be accessed 
-# only if a user is signed-in (if we have user information in session).
-@app.route('/account_page')
-def account_page():
-    connection = get_flask_database_connection(app)
-    repository = PostRepository(connection)
-    if 'user_id' not in session:
-        # No user id in the session so the user is not logged in.
-        return redirect('/login')
-    else:
-        # The user is logged in, display their account page.
-        user_id = session['user_id']
-        posts = repository.find_by_user(user_id)
-        return render_template('account.html', posts=posts)
-
-@app.route('/account_post', methods=['POST'])
-def account_post():
-    connection = get_flask_database_connection(app)
-    repository = PostRepository(connection)
-    # sanitize user input
-    message = sanitizer.sanitize(request.form.get('message'))
-    tag = sanitizer.sanitize(request.form.get('tag'))
-    email = repository.find_email_by_username(tag)
-    # create a post object
-    t = datetime.now()
-    time = t.strftime("%H:%M:%S")
-    user_id = session['user_id']
-    username = repository.find_username_with_userid(user_id)
-    post = Post(None, message, time, user_id, username)
-    # if another user is tagged in their post notify them with an email
-    if email is not None:
-        msg = Message(subject='You were tagged in a post on Chitter', body=f'{username} tagged you in their post. Here is what they wrote:\n\n{message}', sender = 'hello@chitter.com', recipients = [f'{email}'])
-        mail.send(msg)  
-    repository.create(post)
-    return redirect('/')
-
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
