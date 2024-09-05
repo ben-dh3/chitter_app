7c7ddd1757628339cb21c16685c3fe338294daa6
ben-dh3 <benjaminduffield1997@gmail.com>
2024-09-05 14:37:59 +0100

    update three scene

--- static/js/scene.js
+++ static/js/scene.js
@@ -8,10 +8,10 @@ async function loadShader(url) {
 (async function init() {
     const canvas = document.getElementById('threejs-canvas');
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color('#F1F1F5');
+    scene.background = new THREE.Color('#000000');
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 6);
+    camera.position.set(0, 0, 5);
 
     const renderer = new THREE.WebGLRenderer({ canvas });
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -40,7 +40,7 @@ async function loadShader(url) {
             uShininess: { value: 40.0 },
             uDiffuseness: { value: 0.2 },
             uFresnelPower: { value: 8.0 },
-            uLight: { value: new THREE.Vector3(-1.0, 1.0, 1.0) },
+            uLight: { value: new THREE.Vector3(2, 0, 1) },
             winResolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
         }
     });
@@ -52,32 +52,35 @@ async function loadShader(url) {
 
     const textGeometry = new THREE.TextGeometry('MAKE\nYOUR\nMARK', {
         font: font,
-        size: 3.0,
+        size: 0.8,
         height: 0,
     });
-    const textMaterial = new THREE.MeshStandardMaterial({ color: 'black' });
+    const textMaterial = new THREE.MeshStandardMaterial({ color: 'white' });
     const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    textMesh.position.set(-3, 4.5, -4); 
-    scene.add(textMesh);
-
+    textMesh.position.set(0, 1.0, 0); 
+    scene.add(textMesh)
 
     // torus mesh
-    const torusGeometry = new THREE.TorusGeometry(1.2, 0.5, 12, 48);
+    const torusGeometry = new THREE.TorusGeometry(0.6, 0.25, 12, 48);
     const torusMesh = new THREE.Mesh(torusGeometry, shaderMaterial);
-    torusMesh.position.set(0, 0, -2);
+    torusMesh.position.set(0, 0.5, 0.5);
     scene.add(torusMesh);
 
+    // icosahedron mesh
+    const icosahedronGeometry = new THREE.IcosahedronGeometry( 0.22, 5);
+    const icosahedronMesh = new THREE.Mesh(icosahedronGeometry, shaderMaterial);
+    icosahedronMesh.position.set(0.1, -0.7, 2.8);
+    scene.add(icosahedronMesh);
+
     // cylinder mesh
-    const cylinderGeometry = new THREE.CylinderGeometry( 1.2, 1.2, 2.4, 48, 48 );
+    const cylinderGeometry = new THREE.CylinderGeometry( 0.1, 0.1, 1, 32);
     const cylinderMesh = new THREE.Mesh(cylinderGeometry, shaderMaterial);
-    cylinderMesh.position.set(3, 10, -2);
+    cylinderMesh.position.set(0.5, -0.1, 3.0);
     scene.add(cylinderMesh);
 
-    // ocosohedron mesh
-    const octahedronGeometry = new THREE.OctahedronGeometry( 2.0, 0);
-    const octahedronMesh = new THREE.Mesh(octahedronGeometry, shaderMaterial);
-    octahedronMesh.position.set(-2, -3, -2);
-    scene.add(octahedronMesh);
+    // light
+    const light = new THREE.AmbientLight( 0xffffff, 1 )
+    scene.add(light)
 
     const clock = new THREE.Clock();
     function animate() {
@@ -96,17 +99,15 @@ async function loadShader(url) {
         shaderMaterial.uniforms.uTexture.value = mainRenderTarget.texture;
         shaderMaterial.side = THREE.FrontSide;
 
-        torusMesh.position.y = 3 + Math.sin(elapsedTime * 0.4) * 0.5;
-        torusMesh.rotation.x = Math.sin(elapsedTime * 0.1) * 0.5;
-        torusMesh.rotation.y = Math.sin(elapsedTime * 0.1) * 0.5;
+        torusMesh.position.y = Math.sin(elapsedTime * 0.1 * 0.5) * 0.5;
+        torusMesh.rotation.x += 0.001;
+        torusMesh.rotation.y += 0.001;
 
-        cylinderMesh.position.y = 0 + Math.sin(elapsedTime * 0.4) * 0.5;
-        cylinderMesh.rotation.x = Math.sin(elapsedTime * 0.1) * 0.5;
-        cylinderMesh.rotation.y = Math.sin(elapsedTime * 0.1) * 0.5;
-
-        octahedronMesh.position.y = -4 + Math.sin(elapsedTime * 0.4) * 0.5;
-        octahedronMesh.rotation.x = Math.sin(elapsedTime * 0.1) * 0.5;
-        octahedronMesh.rotation.y = Math.sin(elapsedTime * 0.1) * 0.5;
+        icosahedronMesh.position.y = Math.sin(elapsedTime * 0.1 * 0.5 + (Math.PI / 2)) * 0.5;
+   
+        cylinderMesh.position.y = Math.sin(elapsedTime * 0.1 * 0.5 + Math.PI) * 0.5;
+        cylinderMesh.rotation.x += 0.001;
+        cylinderMesh.rotation.y += 0.001;
 
         renderer.setRenderTarget(null);
         renderer.render(scene, camera);
